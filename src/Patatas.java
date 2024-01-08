764e0d2f26a404684e482249c205e562ca785650
MauricioResmar <mauricioremar@gmail.com>
2024-01-08 11:06:58 -0500

    Implemento la respectiva Clase Pojo Patatas, mediante la cual definimos los atributos del objeto, los cuales encapsularemos declarandolos como privados y haciendo uso de los respectivos metodos getter / setter.

--- src/Patatas.java
+++ src/Patatas.java
@@ -1,2 +1,54 @@
+
+// POJO Class Patatas
 public class Patatas {
+
+    private String patatas = "Sin Patatas";
+
+    private String proteinas = "Sin Proteinas";
+
+    private String salsas = "Sin Salsas";
+
+    private String toppings = "Sin Toppings";
+
+    public String getPatatas() {
+        return patatas;
+    }
+
+    public void setPatatas(String patatas) {
+        this.patatas = patatas;
+    }
+
+    public String getProteinas() {
+        return proteinas;
+    }
+
+    public void setProteinas(String proteinas) {
+        this.proteinas = proteinas;
+    }
+
+    public String getSalsas() {
+        return salsas;
+    }
+
+    public void setSalsas(String salsas) {
+        this.salsas = salsas;
+    }
+
+    public String getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(String toppings) {
+        this.toppings = toppings;
+    }
+
+    public void print() {
+        System.out.println(
+            "Construccion de patatas finalizada! " + "\n" +
+                    "Patatas: " + patatas +
+                    " - Proteinas: " + proteinas +
+                    " - Salsas: " + salsas +
+                    " - Toppings: " + toppings);
+
+    }
 }
